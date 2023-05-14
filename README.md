e54ff2127c34b29773f193ad97ef193dc17c6b6e
olga sherstnova <sherstnova.olga@mail.ru>
2023-05-14 21:29:25 +0500

    Step_3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SherOV3005/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/SherOV3005/frontend-project-46/actions)
+[![asciicast](https://asciinema.org/a/wlhg9WQKszGoiWI6IsnR9zlzF.svg)](https://asciinema.org/a/wlhg9WQKszGoiWI6IsnR9zlzF)
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { resolve } from 'path';
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+import stringify from 'stringify.js';
+const getFixturePath = (filepath) => resolve(process.cwd(), filepath);
+const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
+const json = (tree) => JSON.stringify(tree, null, ' '.repeat(2));
+const genDiff = (filepath1, filepath2) => {
+const pathFile1 = getFixturePath (filepath1);
+const pathFile2 = getFixturePath (filepath2);
+const dataFile1 = readFileSync(pathFile1, 'utf-8');
+const dataFile2 = readFileSync(pathFile2, 'utf-8');
+const data1 = JSON.parse(dataFile1);
+const data2 = JSON.parse(dataFile2);
+const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  const resultArr= {};
+  const result = keys.map((key) => {
+    if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+     resultArr[`+ ${key}`] = data2[key];
+    }
+    if (!Object.hasOwn(data2, key) && Object.hasOwn(data1, key)) {
+      resultArr[`- ${key}`] = data1[key];
+      }
+    if ((Object.hasOwn(data2, key) && Object.hasOwn(data1, key)) && !_.isEqual(data1[key], data2[key])) {
+      resultArr[`- ${key}`] = data1[key];
+      resultArr[`+ ${key}`] = data2[key];
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+     resultArr[`  ${key}`] = data1[key];
+    }
+ });
+  const str = JSON.stringify(resultArr, null, ' ');
+  const itogStr = str.replace(/"/gi, '').replace(/,/gi, '');
+  return(itogStr);
+  };
+export default genDiff;
