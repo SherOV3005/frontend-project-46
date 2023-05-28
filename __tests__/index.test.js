cf6d18ba7a177a6aa83dc7e842ae087ca71bb3f2
olga sherstnova <sherstnova.olga@mail.ru>
2023-05-28 22:16:44 +0500

    Step_5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,13 +9,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-const expected = readFile('result.txt');
-const first = getFixturePath('file1.json');
-const second = getFixturePath('file2.json');
-test('difference test', () => {
-const actual = genDiff(first, second);
-    expect(actual).toEqual(expected);
+//test('difference test', () => {
+//const expected = readFile('result.txt');
+//const first = getFixturePath('file1.json');
+//const second = getFixturePath('file2.json');
+//const actual = genDiff(first, second);
+//expect(actual).toEqual(expected);
+//});
+
+test('file json', () => {
+  const filename1 = getFixturePath('file1.json');
+  const filename2 = getFixturePath('file2.json');
+  //const resultname = getFixturePath('result.txt');
+  const result = readFile('result.txt');
+  expect(genDiff(filename1, filename2)).toEqual(result);
 });
+
+test('file yaml', () => {
+  const filename1 = getFixturePath('file1.yaml');
+  const filename2 = getFixturePath('file2.yaml');
+  //const resultname = getFixturePath('result.txt');
+  const result = readFile('result.txt');
+  expect(genDiff(filename1, filename2)).toEqual(result);
+});
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import { resolve } from 'path';
-//import _ from 'lodash';
+import { resolve, extname } from 'path';
 import { readFileSync } from 'fs';
 import fs  from 'fs';
 import buildTree from './buildTree.js';
+import parsers from './parse.js';
 
+  const getFormat = (filepath) => (extname(filepath)).slice(1);
   const getFixturePath = (filepath) => resolve(process.cwd(), filepath);
-  
   const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-  
   const genDiff = (filepath1, filepath2) => {
   const pathFile1 = getFixturePath(filepath1);
   const pathFile2 = getFixturePath(filepath2);
   const dataFile1 = readFile(pathFile1);
   const dataFile2 = readFile(pathFile2);
-  const data1 = JSON.parse(dataFile1);
-  const data2 = JSON.parse(dataFile2);
+  //const data1 = JSON.parse(dataFile1);
+  //const data2 = JSON.parse(dataFile2);
+  const data1 = parsers(dataFile1, getFormat(filepath1));
+  const data2 = parsers(dataFile2, getFormat(filepath2));
   const informationDiff = buildTree(data1, data2);
 return informationDiff;
 };
